eb0f60bb5a3529909ac9314d8841b09c118d137c
aaa <mauro.temp@gmail.com>
2022-10-30 17:17:04 -0400

    removing github actions

--- release.sh
+++ release.sh
@@ -5,7 +5,9 @@ IMAGE_NAME=universal-inbound-agent
 
 image=$IMAGE_REPOSITORY/$IMAGE_NAME
 
-docker build --platform linux/amd64 --no-cache -t :latest -t ghcr.io/mauroarias/universal-inbound-agent:$(cat version) .
-docker push build --platform linux/amd64 --no-cache -t $image:latest -t $image:$(cat version) .
+version=$(cat version)
+
+echo "building $image, latest and $version"
+docker build --platform linux/amd64 --no-cache -t $image:latest -t $image:$version .
 docker push $image:latest
-docker push $image:$(cat version)
+docker push $image:$version
